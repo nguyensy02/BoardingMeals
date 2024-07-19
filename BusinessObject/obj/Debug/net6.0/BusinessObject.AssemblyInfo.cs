4ce689cd50e4334b633e26be1fc75c6ebdf52a72
CoKyAi08 <minh04112002@gmail.com>
2024-07-19 08:57:57 +0700

    add some API

--- BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
+++ BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00eacc8df3403159fa7f3b33a66f0555eba9b44b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60829c3f4e1fbdbbf77cc831f1c7acd4878924a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
