e1ae1a4b2d02126bfca197025c31793ef0a33bd7
nguyensy20 <93036798+nguyensy02@users.noreply.github.com>
2024-07-19 16:28:47 +0700

    ok ok

--- BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
+++ BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60829c3f4e1fbdbbf77cc831f1c7acd4878924a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
